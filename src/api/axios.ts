8a459ca53d9a942133b939410df69c783f87da1e
mark <marknguyenanl@gmail.com>
2025-11-24 15:25:50 +0700

    fix: vite app name and base url var

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useAuthStore } from '@/stores/auth'
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 10000
 })
 
