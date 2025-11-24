fd616f36018bb11cb963535de060a30c3179081f
mark <marknguyenanl@gmail.com>
2025-11-24 11:13:52 +0700

    chore: add env baseUrl axios VITE.API.URL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useAuthStore } from '@/stores/auth'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 10000
 })
 
