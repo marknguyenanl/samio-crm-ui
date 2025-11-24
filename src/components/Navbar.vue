72940d399d63a1163d0e0681abe2e5f018ad12d8
mark <marknguyenanl@gmail.com>
2025-11-24 12:39:38 +0700

    fix env variable

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="flex justify-between mb-2 p-2 px-4 bg-gray-800 text-white">
     <ul class="flex-row flex gap-4">
       <li><RouterLink :to="{ name: 'home' }">Homepage</RouterLink></li>
-      <li><RouterLink :to="{ name: 'docs' }">Docs</RouterLink></li>
+      <!-- <li><RouterLink :to="{ name: 'docs' }">Docs</RouterLink></li> -->
       <li><RouterLink :to="{ name: 'pricing' }">Pricing</RouterLink></li>
     </ul>
     <ul class="flex-row flex gap-4">
