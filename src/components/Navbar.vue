ceb414a4263a6ff39834f7f9d186d01cca0cf5ba
mark <marknguyenanl@gmail.com>
2025-11-25 10:20:51 +0700

    chore: minors

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,19 @@
 <template>
-  <nav class="flex justify-between mb-2 p-2 px-4 text-[#01204E] shadow-lg bg-[#f8fbff]">
-    <ul class="flex-row flex gap-4 items-center">
-      <li>
-        <RouterLink :to="{ name: 'home' }">
-          <img class="h-12 object-cover" src="/Logo.svg" alt="Anros logo" />
-        </RouterLink>
-      </li>
-      <!-- <li><RouterLink :to="{ name: 'docs' }">Docs</RouterLink></li> -->
-      <!-- <li><RouterLink :to="{ name: 'pricing' }">Pricing</RouterLink></li> -->
-    </ul>
-    <ul class="flex-row flex items-center gap-4">
-      <li><RouterLink :to="{ name: 'login' }">Login</RouterLink></li>
-      <li><RouterLink :to="{ name: 'register' }">Register</RouterLink></li>
-    </ul>
+  <nav class="text-[#01204E] shadow-lg py-2 bg-[#f8fbff]">
+    <div class="container flex items-center justify-between mx-auto">
+      <ul class="flex-row flex gap-4 items-center pb-1">
+        <li>
+          <RouterLink :to="{ name: 'home' }">
+            <img class="h-14 object-cover" src="/Logo.svg" alt="Anros logo" />
+          </RouterLink>
+        </li>
+        <!-- <li><RouterLink :to="{ name: 'docs' }">Docs</RouterLink></li> -->
+        <!-- <li><RouterLink :to="{ name: 'pricing' }">Pricing</RouterLink></li> -->
+      </ul>
+      <ul class="flex-row flex items-center gap-4">
+        <li><RouterLink :to="{ name: 'login' }">Login</RouterLink></li>
+        <li><RouterLink :to="{ name: 'register' }">Register</RouterLink></li>
+      </ul>
+    </div>
   </nav>
 </template>
