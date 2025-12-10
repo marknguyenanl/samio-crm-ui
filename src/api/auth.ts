19af3e460fbb37bb2ac210ab5cabbd5a0baca310
mark <marknguyenanl@gmail.com>
2025-12-10 23:11:33 +0700

    chore: fix demo api

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -17,8 +17,12 @@ export async function loginAPI(data: LoginPayload) {
 }
 
 export async function demoLoginAPI() {
+  const demo = {
+    'email': 'demo@samiohome.com',
+    'password': 'demoaccount',
+  };
   try {
-    const response = await axios.post(`${import.meta.env.VITE_API_URL}/v1/demo`)
+    const response = await api.post('/v1/demo', { demo })
     return response.data
   } catch (error) {
     console.error('error is: ', error)
